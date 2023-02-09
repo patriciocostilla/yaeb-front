1716ce22aa3d492464d50091b2c41bf0bb0c5b91
Patricio Costilla <juan.costilla@santexgroup.com>
2023-02-09 15:12:53 -0300

    adding base-href changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,36 @@
+import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, isDevMode } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { environment as env} from '../environments/environment';
+import { environment as env } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  info = new BehaviorSubject<any>({})
+  info = new BehaviorSubject<any>({});
   infoArray: [string, unknown][] = [];
+  urlArray: [string, unknown][] = [
+    ['location.host', window.location.host],
+    ['location.href', window.location.href],
+  ];
 
   apiUrl = env.API_URL;
   refreshedAt: Date | null = null;
 
-  constructor(private http: HttpClient, private title: Title, private router: Router) {
+  constructor(
+    private http: HttpClient,
+    private title: Title,
+    private router: Router,
+    private location: Location
+  ) {
     this.info.subscribe((newInfo) => {
-      this.infoArray = Object.entries(newInfo)
-    })
+      this.infoArray = Object.entries(newInfo);
+    });
 
     this.refresh();
     setInterval(() => this.refresh(), 5000);
@@ -29,17 +39,26 @@ export class AppComponent {
       this.router.events.subscribe((event) => {
         if (event instanceof NavigationEnd) {
           let currentTitle = this.title.getTitle();
-          this.title.setTitle(`${currentTitle} (dev)`)
+          this.title.setTitle(`${currentTitle} (dev)`);
         }
-      })
+      });
     }
 
+    this.urlArray.push(['angularLocation.path()', this.location.path()]);
+    this.urlArray.push([
+      'angularLocation.prepareExternalUrl()',
+      this.location.prepareExternalUrl(''),
+    ]);
+    this.urlArray.push([
+      "angularLocation.prepareExternalUrl('v2')",
+      this.location.prepareExternalUrl('v2'),
+    ]);
   }
 
   refresh() {
     this.http.get(`${this.apiUrl}/ping`).subscribe((data) => {
       this.info.next(data);
       this.refreshedAt = new Date();
-    })
+    });
   }
 }
